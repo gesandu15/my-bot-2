dfa2d822401a9900989fbc3af9bb51ced23b44ca
gesandu15 <useg329@gmail.com>
2025-08-22 17:08:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "WLwRUTTa#Bl8cDN2IFX3hIqmv-y_Ps3sfDPjBvmGyuwsYEmT89Dg",
+  SESSION_ID: process.env.SESSION_ID || "jWB1gaSJ#Ow1BZvD0lGN3uV6_wyd2Hp8IJl43Nx8LIUX3dE2irTc",
   MONGODB: process.env.MONGODB || "mongodb://mongo:PTrRcvFLmSoHDgITTInweNsiaunZyHzP@turntable.proxy.rlwy.net:21003",
   OWNER_NUM: process.env.OWNER_NUM || "94784525290",
 };
